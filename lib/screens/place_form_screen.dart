d01b47d707aaf8d7a3b22736f4377b427d708d9c
Cleiton Araujo <cleitonamc@gmail.com>
2025-04-13 07:44:05 -0300

    Configurações inicial do formilario para envio de imagens

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import '../widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({super.key});
@@ -10,13 +11,64 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+
+  final _titleController = TextEditingController();
+
+  void _submitForm(){
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _titleController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Novo Lugar'),
       ),
-      body: Center(child: Text('Form!!!'),),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          //Expanded para ocupar o espaço de tela e deixar o butoon no rodapé
+          Expanded(
+            //E o ScrollView é uma proteção para garantir que haja a rolagem dentro desse
+            //bloco, mantendo o button em baixo e n escondendo nenhum objeto da tela
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Titulo',
+                      ),
+                      controller: _titleController,
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            label: Text('Adicionar'),
+            icon: Icon(Icons.add),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.amber,
+              elevation: 0,
+              shape: RoundedRectangleBorder(),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap, 
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
