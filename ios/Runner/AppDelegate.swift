4bbc9edb7a32871aaca20ce74a6d7cebc4be5a96
Cleiton Araujo <cleitonamc@gmail.com>
2025-04-15 21:47:37 -0300

    instalação do http e troca de chave da api

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCskBOpA0Ts6wI9E2KTLdoYcZ7WmD72lrI")
+    GMSServices.provideAPIKey(APIKeys.apiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
